5d1cc4aec1452483272e3169d4bd8a8602881037
Dax <contact@antoniodavide.dev>
2024-10-30 19:04:52 +0100

    chore: updated dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -9,32 +9,32 @@ set(CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake" ${CMAKE_MODULE_PATH})
 function(setup_dependencies)
     # Priority over old Catch2's versions from dependencies
     if(BUILD_TESTING)
-        CPMAddPackage("gh:catchorg/Catch2@3.5.0")
+        CPMAddPackage("gh:catchorg/Catch2@3.7.1")
     endif()
 
     CPMAddPackage("gh:TartanLlama/expected@1.1.0")
     CPMAddPackage("gh:TartanLlama/optional@1.1.0")
-    CPMAddPackage("gh:fmtlib/fmt#10.1.1")
-    CPMAddPackage("gh:eliaskosunen/scnlib@1.1.3")
+    CPMAddPackage("gh:fmtlib/fmt#10.2.1")
+    CPMAddPackage("gh:eliaskosunen/scnlib@3.0.1")
     CPMAddPackage("gh:Dax89/cl@1.0.1")
 
     CPMAddPackage(
         NAME boost_regex
-        GIT_TAG "boost-1.84.0"
+        GIT_TAG "boost-1.86.0"
         GITHUB_REPOSITORY "boostorg/regex"
         OPTIONS "BOOST_REGEX_STANDALONE ON"
     )
 
     CPMAddPackage(spdlog
         NAME spdlog
-        VERSION "1.12.0"
+        VERSION "1.14.1"
         GITHUB_REPOSITORY "gabime/spdlog"
         OPTIONS "SPDLOG_FMT_EXTERNAL ON"
     )
 
     CPMAddPackage(
         NAME RapidJSON
-        GIT_TAG "ab1842a2dae061284c0a62dca1cc6d5e7e37e346"
+        GIT_TAG "858451e5b7d1c56cf8f6d58f88cf958351837e53"
         GITHUB_REPOSITORY "Tencent/rapidjson"
         OPTIONS
             "RAPIDJSON_BUILD_TESTS OFF"
